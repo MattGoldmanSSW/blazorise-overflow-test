e695391a1e832f0d6e9aefceb2e2984ee7695e63
Matt Goldman <mattgoldman1981@gmail.com>
2020-11-24 08:13:43 +1100

    added demo project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Material;
+using Blazorise.Icons.Material;
+
+namespace overflowtest
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBlazorise(options =>
+            {
+                options.ChangeTextOnKeyPress = true;
+            })
+            .AddMaterialProviders()
+            .AddMaterialIcons();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            var host = builder.Build();
+
+            host.Services
+                .UseMaterialProviders()
+                .UseMaterialIcons();
+
+            await host.RunAsync();
+        }
+    }
+}
